ba2cf872a4f7398d665605d1e87a3f27747817f3
xvw <xavier.vdw@gmail.com>
2015-09-15 16:48:01 +0200

    base Wilson formulas

--- src/wilson.erl
+++ src/wilson.erl
@@ -7,5 +7,9 @@
 -author(["Xavier Van de Woestyne"]).
 
 %% Export rules 
--export([]).
+-export([c/4, t/3, q/3]).
 
+
+c(T, Cs, Cl, K) -> Cs * ((K * T)/2) + (Cl / T). 
+t(Cl, Cs, K) -> math:sqrt( (2*Cl) / (Cs * K) ).
+q(Cl, Cs, K) -> math:sqrt( ((2*Cl) * K) / Cs ).
