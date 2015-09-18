b3d38f6d1492ee1d54b51a37ecbec62fe512bd84
altor <arthur.dazemar@free.fr>
2015-09-18 17:27:06 +0000

    add olists:foreach_except

--- src/olists.erl
+++ src/olists.erl
@@ -1,5 +1,5 @@
 -module(olists).
--export([droplast/1]).
+-export([droplast/1, foreach_except/2]).
 
 %% @doc Drops the last element of a List. The list should be non-empty, otherwise the function will crash with a function_clause
 -spec droplast(List :: [T]) -> [T].
@@ -8,3 +8,15 @@ droplast([]) ->
 droplast(L) ->
     [_ | Result] = lists:reverse(L),
     lists:reverse(Result).
+
+%% @doc Calls Fun(Elem) for each element Elem in List except the last. This function is used for its side effects and the evaluation order is defined to be the same as the order of the elements in the list.
+%%      The list should be non-empty, otherwise the function will crash with a function_clause
+-spec(Fun :: fun((Elem :: T) -> term()), List :: [T]) -> ok
+foreach_except(_, []) ->
+    error(function_clause);
+foreach_except(_, [_]) ->
+    ok;
+foreach_except(Fun, [H | T]) ->
+    Fun(H),
+    foreach_except(Fun, T).
+
