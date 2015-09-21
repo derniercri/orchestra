1db9213b6ed9ad56e59e3f72fe26382b26e3fcde
altor <arthur.dazemar@free.fr>
2015-09-21 11:48:23 +0000

    add olists:fold_except2

--- src/olists.erl
+++ src/olists.erl
@@ -1,5 +1,5 @@
 -module(olists).
--export([droplast/1, foreach_except/2, foreach_except2/2]).
+-export([droplast/1, foreach_except/2, foreach_except2/2, foldl_except2/3]).
 
 %% @doc Drops the last element of a List. The list should be non-empty, otherwise the function will crash with a function_clause
 -spec droplast(List :: [T]) -> [T].
@@ -33,3 +33,14 @@ foreach_except2(Fun, [H | T]) ->
     Fun(H),
     foreach_except(Fun, T).
 
+%% @doc Like lists:foldl but don't apply the function on the two last element of the list
+-spec foldl_except2(Fun :: fun((Elem :: T, AccT) -> AccT), Acc0 :: AccT, List :: [T]) -> AccT.
+foldl_except2(_, _, []) -> 
+    error(function_clause);
+foldl_except2(_, _, [_]) -> 
+    error(function_clause);
+foldl_except2(_, Acc, [_, _]) -> 
+    Acc;
+foldl_except2(Fun, Acc0, [H | T]) -> 
+    Acc = Fun(H, Acc0),
+    foldl_except2(Fun, Acc, T).
--- test/olists_test.erl
+++ test/olists_test.erl
@@ -7,3 +7,16 @@ error_droplast_test() ->
 droplast_test() ->
     ?assertEqual([1,2,3], olists:droplast([1,2,3,4]) ),
     ?assertEqual([], olists:droplast([1]) ).
+
+fold_except2_error_empty_test() ->
+    ?assertError(function_clause, olists:foldl_except2(fun(X, _) -> X end, 0, [])).
+
+fold_except2_error_one_elem_test() ->
+    ?assertError(function_clause, olists:foldl_except2(fun(X, _) -> X end, 0, [1])).
+
+fold_except2_two_elem_test() ->
+   ?assertEqual(0, olists:foldl_except2(fun(X, _) -> X end, 0, [1, 2])).
+
+fold_except2_normal_test() ->
+    Fun = fun (X, Acc) -> X + Acc end,
+    ?assertEqual(10 , olists:foldl_except2(Fun, 0, [1, 2, 3, 4, 5, 6])).
